333ed4dfda573aebd0211f2672e73225e0bc5989
js813N <gpflvhxj113@naver.com>
2024-12-12 00:08:19 +0900

    feat : 프로젝트 기본 셋업
    1. .gitignore 추가
    2. FrontController 기능 추가
    3. error.jsp, main.jsp 추가
    4. web.xml에 메인페이지 경로 추가
    5. MainController 추가

---
+++ src/main/java/common/controller/InterCommand.java
@@ -0,0 +1,12 @@
+package common.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface InterCommand {
+
+	/*
+	 * HTTP Method(GET, POST) 요청을 받아 실행하는 메소드
+	 */
+	void execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
