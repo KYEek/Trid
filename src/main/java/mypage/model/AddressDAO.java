ed758e0a99c9c9e369b80b39ccf3109676eb8dd2
KYEek <dltnstls89@hotmail.com>
2024-12-21 15:14:32 +0900

    build : 필요 파일들 추가
    최종결과가 아닌 중간 진행입니다

--- src/main/java/mypage/model/AddressDAO.java
+++ src/main/java/mypage/model/AddressDAO.java
@@ -1,5 +1,11 @@
 package mypage.model;
 
+import java.sql.SQLException;
+
+import org.json.JSONObject;
+
 public interface AddressDAO {
+
+	JSONObject selectAddrs(int pk_member_no) throws SQLException;
 	
 }
---
+++ src/main/java/mypage/model/AddressDAO_imple.java
@@ -0,0 +1,70 @@
+package mypage.model;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.json.JSONObject;
+
+import common.Constants;
+import util.security.AES256;
+
+public class AddressDAO_imple implements AddressDAO {
+
+	private DataSource ds;  // DataSource ds 는 아파치톰캣이 제공하는 DBCP(DB Connection Pool)이다. 
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private AES256 aes;
+	
+	// 생성자
+	public AddressDAO_imple() {
+		
+		try {
+			Context initContext = new InitialContext();
+		    Context envContext  = (Context)initContext.lookup("java:/comp/env");
+		    ds = (DataSource)envContext.lookup("jdbc/myoracle");
+		    
+		    aes = new AES256(Constants.KEY);
+		    // SecretMyKey.KEY 은 우리가 만든 암호화/복호화 키이다.
+		    
+		} catch(NamingException e) {
+			e.printStackTrace();
+		} catch(UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// 사용한 자원을 반납하는 close() 메소드 생성하기
+	private void close() {
+		try {
+			if(rs    != null) {rs.close();	  rs=null;}
+			if(pstmt != null) {pstmt.close(); pstmt=null;}
+			if(conn  != null) {conn.close();  conn=null;}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}// end of private void close()---------------
+	
+	
+	
+	@Override
+	public JSONObject selectAddrs(int pk_member_no) throws SQLException {
+		
+		JSONObject json = new JSONObject();
+		conn = ds.getConnection();
+		
+		String sql = "";
+		return json;
+	}
+
+}
